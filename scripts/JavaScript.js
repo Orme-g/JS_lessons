5dac590f612f8e862be811e356ad83df4e5f6413
Ilya <ilya.grafkin@gmail.com>
2021-11-29 15:10:21 +0300

    First commit

---
+++ scripts/JavaScript.js
@@ -0,0 +1,42 @@
+'use strict';
+// const obj = {
+//     name: 'Jonny',
+//     surname: 'Sins',
+//     age: 45,
+//     isMarried: false
+// };
+
+// let arr = [
+//     'car', 'house', '1000000', 'fields'
+// ];
+
+// alert ('Hello!');
+
+// const result = confirm('Are you ready?');
+// console.log(result);
+
+// const answer = prompt ('Вам есть 18 лет?', '18'); 
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt ('Как ваше имя?', '');
+// answers[1] = prompt ('Сколько вам лет?', '');
+// answers[2] = prompt ('Откуда вы?', '');
+
+// console.log(typeof(answers));
+
+
+// const user = 'Ilya';
+// alert (`Hello, ${user}`);
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+console.log( 4 === '4');
