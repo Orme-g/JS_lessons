01eca65e1965636cb505a039e78da6086a598f9b
Ilya <ilya.grafkin@gmail.com>
2021-11-29 18:04:33 +0300

    Start DB

--- scripts/JavaScript.js
+++ scripts/JavaScript.js
@@ -39,4 +39,23 @@
 // console.log(++incr);
 // console.log(--decr);
 
-console.log( 4 === '4');
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    prival: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько оцените его', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
